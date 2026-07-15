3a5843197e3df00dc4f8b78909b5529e95cd1478
Codingwithriha <rihashehzadi2003@gmail.com>
2026-07-15 20:02:23 +0500

    feat(server): initialize server with TypeScript, MongoDB, and Redis integration
    - Added package.json for server dependencies and scripts
    - Created server.ts to set up the Express server and connect to the database
    - Configured TypeScript with tsconfig.json for compilation settings
    - Implemented MongoDB connection logic in utils/db.ts
    - Added Redis client initialization in utils/redis.ts

---
+++ server/app.ts
@@ -0,0 +1,30 @@
+require('dotenv').config();
+import express, { NextFunction, Request, Response } from 'express';
+export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+// body parser
+app.use(express.json({limit: '50mb'}));
+
+//cookie-parser
+app.use(cookieParser());
+
+//cors -> cross origin resource sharing
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+//testing root
+app.get("/test" , (req:Request, res:Response, next:NextFunction) => {
+    res.status(200).json({
+        message: "Api is running"
+    });
+});
+
+//unknown routes
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+})
