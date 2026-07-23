20be250d01882c7f41aaa036107948b9c6eb8d57
Codingwithriha <rihashehzadi2003@gmail.com>
2026-07-23 14:15:45 +0500

    feat(course): update comment schema to include question field and add question route

--- server/models/course.model.ts
+++ server/models/course.model.ts
@@ -2,8 +2,8 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 import { IUser } from "./user.model";
 interface IComment extends Document {
   user: IUser;
-  comment: string;
-  commentReplies?: IComment[];
+  question: string;
+  questionReplies?: IComment[];
 }
 
 interface IReview extends Document {
@@ -63,8 +63,8 @@ const linkSchema = new Schema<ILink>({
 const commentSchema = new Schema<IComment>(
   {
     user: Object,
-    comment: String,
-    commentReplies: [Object],
+    question: String,
+    questionReplies: [Object],
   },
   { timestamps: true },
 );
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getAllCourse, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addQuestion, editCourse, getAllCourse, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
@@ -32,5 +32,10 @@ courseRouter.get(
     "/get-course-content/:id",
     isAuthenticated,
     getCourseByUser
+)
+courseRouter.put(
+    "/add-question",
+    isAuthenticated,
+    addQuestion
 )
 export default courseRouter;
