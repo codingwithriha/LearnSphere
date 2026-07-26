7727cc05b4a2949a9a509f8241fa73a173ac27d5
Codingwithriha <rihashehzadi2003@gmail.com>
2026-07-26 09:18:00 +0500

    feat(admin): add endpoints to retrieve all courses and users for admin access

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,16 +1,35 @@
-import express from 'express';
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updateUserInfo, updatePassword, updateProfilePicture } from '../controllers/user.controller';
-import { authorizeRoles, isAuthenticated } from '../middleware/auth';
+import express from "express";
+import {
+  activateUser,
+  getUserInfo,
+  loginUser,
+  logoutUser,
+  registrationUser,
+  socialAuth,
+  updateAccessToken,
+  updateUserInfo,
+  updatePassword,
+  updateProfilePicture,
+  getAllUsers,
+} from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const userRouter = express.Router();
-userRouter.post('/registration', registrationUser);
-userRouter.post('/activate-user', activateUser);
-userRouter.post('/login', loginUser);
-userRouter.get('/logout', isAuthenticated, logoutUser);
-userRouter.get('/refresh', updateAccessToken);
-userRouter.get('/me', isAuthenticated, getUserInfo);
-userRouter.post('/social-auth', socialAuth);
-userRouter.put('/update-user-info', isAuthenticated, updateUserInfo);
-userRouter.put('/update-user-password', isAuthenticated, updatePassword);
-userRouter.put('/update-user-avatar', isAuthenticated, updateProfilePicture);
+userRouter.post("/registration", registrationUser);
+userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
+userRouter.get("/refresh", updateAccessToken);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
+userRouter.get(
+  "/get-users",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getAllUsers
+);
+
 export default userRouter;
