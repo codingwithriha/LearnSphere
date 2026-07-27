7267f87a11572b29a95fa76fd0e26c13729f83f5
Codingwithriha <rihashehzadi2003@gmail.com>
2026-07-27 11:26:24 +0500

    fix(user): enable caching of user data in updateAccessToken function

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -238,7 +238,7 @@ export const updateAccessToken = CatchAsyncError(
       req.user = user;
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
-      //await redis.set(user._id, JSON.stringify(user), "EX", 604800);
+      await redis.set(user._id, JSON.stringify(user), "EX", 604800);
 
       return res.status(200).json({
         status: "success",
